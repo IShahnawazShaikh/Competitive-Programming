738a8f4196e61f5625af58a4eddc0989f807642f
shahnawazshaikh123 <32239343+shahnawazshaikh123@users.noreply.github.com>
2020-06-22 16:38:30 +0530

    Maximum pair og GCD

---
+++ Code Forces/P1370A.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main(){
+int n,i,result,temp,j,t;
+cin>>t;
+while(t>0){
+cin>>n;
+cout<<n/2;
+cout<<endl;
+t--;
+}
+
+return 0;
+}
