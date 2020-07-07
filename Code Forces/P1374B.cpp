fbe3ad00973e47fe471a41dce234df6f7ee9d9fc
shahnawazshaikh123 <32239343+shahnawazshaikh123@users.noreply.github.com>
2020-07-07 23:13:43 +0530

    count numbers of moves from n to 1
    multiply by 2 and divide by six if n%6==0
    find minimum moves

--- Code Forces/P1374B.cpp
+++ Code Forces/P1374B.cpp
@@ -1,38 +1,38 @@
 /*
-    ******
-    
-       
-
-    ******
+------------------------------|بسم الله الرحمن الرحيم|------------------------------//
+------------"IN THE NAME OF ALLAH, THE MOST GRACIUOS, THE MOST MERCIFUL"-------------//
+__________________________________ShahnawazShaikh_____________________________________
 */
 #include<iostream>
-#define ll long long int
+#include<cmath>
 using namespace std;
-
-#define int int64_t
+#define ll long long
 int32_t main(){
- ios_base::sync_with_stdio(false);
- cin.tie(NULL);	
- int t;
- cin>>t;
- while(t--){
- int x;
- int y;
- int n;
- int a;
- int b;
- int rem;
- cin>>x>>y>>n;
-    int temp=n;
- 	rem=n%x;
- 	n-=rem;
-    rem=n-abs(x-y%x);
-     //cout<<rem+x<<endl;
-     if((rem+x)<=temp)
-     	 cout<<rem+x;
-     else
-     	cout<<rem;
- cout<<endl;	
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);	
+int t;
+cin>>t;
+while(t--){
+ ll int n;
+ ll int moves=0;
+ cin>>n;
+ if(n==1)
+ 	moves=0;
+ else{
+ while(n!=1){
+   moves+=1;
+   if(n==2 | n>1000000000){
+ 	moves=-1;
+ 	break;
+   }
+   else if(n%6==0)
+      n/=6;
+   else
+       n*=2;
+  }
  }
+ cout<<moves;
+ cout<<endl;	
+}
  return 0;	
 }
