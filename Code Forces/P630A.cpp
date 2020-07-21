70ac5a3e881b3251413f6f524d9752a58b5d7515
shahnawazshaikh123 <32239343+shahnawazshaikh123@users.noreply.github.com>
2020-07-21 22:42:34 +0530

    Again 25 problem

---
+++ Code Forces/P630A.cpp
@@ -0,0 +1,19 @@
+/*
+------------------------------|بسم الله الرحمن الرحيم|------------------------------//
+------------"IN THE NAME OF ALLAH, THE MOST GRACIUOS, THE MOST MERCIFUL"-------------//
+__________________________________ShahnawazShaikh_____________________________________
+*/
+
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+#define ll long long
+int32_t main(){
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);	
+ll int n;
+cin>>n;
+cout<<25;
+return 0;	
+}
