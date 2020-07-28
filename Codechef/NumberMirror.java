64389e8c0a889da49d7aca9e2f43317244066a80
shahnawazshaikh123 <32239343+shahnawazshaikh123@users.noreply.github.com>
2020-07-28 18:50:45 +0530

    Mirror Number (Same Number)

---
+++ Codechef/NumberMirror.java
@@ -0,0 +1,15 @@
+/*
+------------------------------|بسم الله الرحمن الرحيم|------------------------------//
+------------"IN THE NAME OF ALLAH, THE MOST GRACIUOS, THE MOST MERCIFUL"-------------//
+__________________________________ShahnawazShaikh_____________________________________
+*/
+import java.io.*;
+import java.util.*;
+class CodeChef{
+	public static void main(String[] x) throws java.lang.Exception{
+	Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    System.out.println(n);
+	}
+
+}
