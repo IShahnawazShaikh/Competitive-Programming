71c861bbc77b549747c286f964eb44cb05f5ea04
shahnawazshaikh123 <32239343+shahnawazshaikh123@users.noreply.github.com>
2020-07-28 18:50:29 +0530

    Find the remainder

---
+++ Codechef/FindRemainder.java
@@ -0,0 +1,22 @@
+/*
+------------------------------|بسم الله الرحمن الرحيم|------------------------------//
+------------"IN THE NAME OF ALLAH, THE MOST GRACIUOS, THE MOST MERCIFUL"-------------//
+__________________________________ShahnawazShaikh_____________________________________
+*/
+import java.io.*;
+import java.util.*;
+class FindRemainder{
+	public static void main(String[] x) throws java.lang.Exception{
+	Scanner sc=new Scanner(System.in);
+    
+    int t=sc.nextInt();
+    
+    while(t>0){
+    int a=sc.nextInt();
+    int b=sc.nextInt();
+    System.out.println(a%b); 
+    t--;	
+    }
+  }
+
+}
