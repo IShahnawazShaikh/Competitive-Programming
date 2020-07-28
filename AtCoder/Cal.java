e05c1b088bf193bcacf6383fa3ae9f82a1bfb2e8
shahnawazshaikh123 <32239343+shahnawazshaikh123@users.noreply.github.com>
2020-07-28 18:49:06 +0530

    Calculate a+a*a+a*a*a

---
+++ AtCoder/Cal.java
@@ -0,0 +1,17 @@
+/*
+------------------------------|بسم الله الرحمن الرحيم|------------------------------//
+------------"IN THE NAME OF ALLAH, THE MOST GRACIUOS, THE MOST MERCIFUL"-------------//
+__________________________________ShahnawazShaikh_____________________________________
+*/
+import java.io.*;
+import java.util.*;
+class Cal{
+	public static void main(String[] x) throws java.lang.Exception{
+	Scanner sc=new Scanner(System.in);
+    short a=sc.nextShort();
+    System.out.println(a+a*a+a*a*a);
+	
+
+	}
+
+}
