4b548295ee67e8d96bfe5daa564b48d75a2aace2
shahnawazshaikh123 <32239343+shahnawazshaikh123@users.noreply.github.com>
2020-08-10 20:43:50 +0530

    Codeforces Question

---
+++ Codeforces/P1393A.cpp
@@ -0,0 +1,23 @@
+/*
+------------------------------|بسم الله الرحمن الرحيم|------------------------------//
+------------"IN THE NAME OF ALLAH, THE MOST GRACIUOS, THE MOST MERCIFUL"-------------//
+__________________________________ShahnawazShaikh_____________________________________
+*/
+
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+#define ll long long
+int32_t main(){
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);	
+int t;
+cin>>t;
+while(t--){
+ll int n;
+cin>>n;
+cout<<n/2+1<<endl;	
+}
+return 0;	
+}
