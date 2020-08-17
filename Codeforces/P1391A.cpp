a056f2c59f8cb6ef21b64ff2885774813dbdb3ae
shahnawazshaikh123 <32239343+shahnawazshaikh123@users.noreply.github.com>
2020-08-17 12:15:33 +0530

    Find Permutaion

---
+++ Codeforces/P1391A.cpp
@@ -0,0 +1,25 @@
+/*
+------------------------------|بسم الله الرحمن الرحيم|------------------------------//
+------------"IN THE NAME OF ALLAH, THE MOST GRACIUOS, THE MOST MERCIFUL"-------------//
+__________________________________ShahnawazShaikh_____________________________________
+*/
+
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+#define int int64_t
+int32_t main(){
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);	
+int t;
+cin>>t;
+while(t--){
+int n,i;
+cin>>n;
+for(i=1;i<=n;i++)
+	cout<<i<<" ";
+cout<<endl;
+}
+return 0;	
+}
